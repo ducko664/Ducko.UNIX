2a9249bc4f3b56288ea7055af02c2358389af22f
ducko664 <137336649+ducko664@users.noreply.github.com>
2026-05-28 22:24:23 -0700

    Add files via upload

---
+++ ty.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    printf(1, "Thanks for using my OS!!!\n");
+    printf(1, "Luv ya :P\n");
+    exit(); // unix things must end in exit!!! never firget
+}
---
+++ ty.d
@@ -0,0 +1,1 @@
+ty.o: ty.c /usr/include/stdc-predef.h types.h stat.h user.h
